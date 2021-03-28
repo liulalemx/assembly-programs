149bab1bec7cf4057ee4d6c836e757499fe27aea
liul <48413031+liulaz@users.noreply.github.com>
2021-03-28 22:28:38 +0300

    Initial Commit

---
+++ HelloWorld.asm
@@ -0,0 +1,26 @@
+TITLE Hello World
+
+;Program Desctiption: This program prints hello world
+;Author: Liul Alemayehu
+;Creation Date: 3/28/2021
+
+.MODEL SMALL                                 ; com program, Memory Descriptor (Tiny | Small | Medium | Large)
+
+.DATA                                        ; Beginning of data segment where data is stored
+    msg db "Hello World!$"                   ; Declare variable msg and assigned it a value ($ used to indicate end of string)
+
+.CODE                                        ; Beginning of code segment
+
+main PROC                                    
+    MOV Ax,@DATA                             ; Moving address of data (data segment) to Ax register
+    MOV Ds,Ax                                ; Initializing of Data segment register (Ds) from Accumilator register (Ax) 
+    LEA Dx,msg                               ; Loading the effective address of our msg to the Data register (Dx)
+    mov ah,09h                               ; Print function
+    int 21h                                  ; Does it!
+                                             
+    MOV Ah,4Ch                               ; Exit
+    INT 21h                                  ; Does it!
+main ENDP
+
+END main    
+
---
+++ ProgramTemplate.asm
@@ -0,0 +1,17 @@
+TITLE Program Template
+
+;Program Desctiption:
+;Author:
+;Creation Date:
+;Revisions:
+;Date:
+
+.data
+    ;(insert variables here)
+.code
+main PROC
+    ;(insert executable instructions here)
+    exit
+main ENDP
+    ;(insert additional procedures here)
+END main
