6211b5532572643259ee3506822b56fc72b44110
liul <48413031+liulaz@users.noreply.github.com>
2021-03-31 18:18:17 +0300

    Added Loop and Print

--- 6.LoopAndPrint.asm
+++ 6.LoopAndPrint.asm
@@ -1,13 +1,13 @@
-TITLE ReadThenPrint
+TITLE LoopAndPrint
 
-;Program Desctiption: This program reads input from keyboard and prints it on the screen. It stops execution when ENTER key is pressed
+;Program Desctiption: This program loops through commands a specified amount of time
 ;Author: Liul Alemayehu
 
 .MODEL SMALL                                 ; com program, Memory Descriptor (Tiny | Small | Medium | Large)
 .STACK 100                                   ; stack
 
 .DATA                                        ; Beginning of data segment where data is stored
-    msg db 'Hi! Please enter a word: ','$'   ; Declare variable msg and assigned it a value ($ used to indicate end of string)
+    msg db 'Loop! ','$'                      ; Declare variable msg and assigned it a value ($ used to indicate end of string)
     newline db 13,10,'$'                     ; Creates newline
 
 .CODE                                        ; Beginning of code segment
@@ -15,28 +15,27 @@ main PROC
     ; Start
     MOV Ax,@DATA                             ; Moving address of data (data segment) to Ax register
     MOV Ds,Ax                                ; Initializing of Data segment register (Ds) from Accumilator register (Ax) 
+    MOV Cx,5                                 ; Set the value of Cx (determines the number of times a loop runs, 5 in this case)
     
+    Messg:                                   ; Message Label
+    ; Display msg
     LEA Dx,msg                               ; Loading the effective address of our msg to the Data register (Dx)
     MOV Ah,09                                ; Print from data 
+    INT 21h                                  ; Does it!                                
+    
+    ; Prints loop number
+    MOV AH,02h                               ; Prints number
+    MOV Dl,Cl                                ; Moves value of Cl to Dl
+    ADD Dl,30h                               ; Converts value to ASCII
     INT 21h                                  ; Does it!
     
-    MOV AH,01h                               ; Reads an input char to the Accumilator register (high)
-    INT 21h                                  ; Does it! (reads charachter into Al)
-
-    MOV Bl,Al                                ; Save character in Bl while we display a Return and Linefeed
-        
-    CMP Al,13                                ; Compare the input in the Accumilator registor to the ASCII value of the ENTER key (13)
-    JE EXIT                                  ; Jumps to 'Exit' label if the CMP is equal
-
     ; Newline
     LEA Dx,newline
     MOV Ah,09
     INT 21h
-                                                                               
-    MOV AH,02                                ; Prints Character
-    MOV Dl,Bl                                ; Access input value from [MOV AH,01] through B1 and move to Dl
-    INT 21h                                  ; Does it!
-    
+                                             ; Jumps program to Messg label
+    LOOP Messg 
+       
     Exit:    
     MOV Ah,4Ch                               ; Exit
     INT 21h                                  ; Does it!
